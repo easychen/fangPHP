937ecc84349573a8c0cd2c18dd08378731c06627
EasyChen <easychen@qq.com>
2018-01-23 09:46:08 +0800

    调整策略，win下不再挂载mysql数据目录到匿名卷

---
+++ livereload.bat
@@ -0,0 +1,5 @@
+netsh interface portproxy add v4tov4 listenport=80 listenaddress=127.0.0.1 connectport=80 connectaddress=192.168.99.100 protocol=tcp
+netsh interface portproxy add v4tov4 listenport=35729 listenaddress=127.0.0.1 connectport=35729 connectaddress=192.168.99.100 protocol=tcp
+
+pause
+netsh interface portproxy delete 
