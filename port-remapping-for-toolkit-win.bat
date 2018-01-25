2f79e00a797cb985e5aecfd85340382f95fdfe3e
EasyChen <easychen@qq.com>
2018-01-25 12:54:47 +0800

    调整win的端口映射

--- port-remapping-for-toolkit-win.bat
+++ port-remapping-for-toolkit-win.bat
@@ -2,4 +2,5 @@ netsh interface portproxy add v4tov4 listenport=80 listenaddress=127.0.0.1 conne
 netsh interface portproxy add v4tov4 listenport=35729 listenaddress=127.0.0.1 connectport=35729 connectaddress=192.168.99.100 protocol=tcp
 
 pause
-netsh interface portproxy delete 
+netsh interface portproxy delete v4tov4 listenport=80 listenaddress=127.0.0.1
+netsh interface portproxy delete v4tov4 listenport=35729 listenaddress=127.0.0.1
